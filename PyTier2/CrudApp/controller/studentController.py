3cf71bad9f333548fb15c220ef7243358cedc656
Anandhakumar <anandhaanavamani@gmail.com>
2026-08-08 16:12:42 +0530

    final

--- PyTier2/CrudApp/controller/studentController.py
+++ PyTier2/CrudApp/controller/studentController.py
@@ -1,22 +1,94 @@
-from flask import request
+# from flask import request,render_template
+# from dataBase.db import cursor,connection
+
+# def getStudents():
+#     cursor.execute("select * From STUDENTS")
+#     stu =  cursor.fetchall()
+#     return render_template(
+#         "index.html",
+#         stu=stu
+#     )
+
+# def addStudent():
+#     data = request.get_json()
+#     sql = """INSERT INTO STUDENTS(STUDENT_NAME,COURSENAME,AGE) VALUES (%s,%s,%s)"""
+#     values = (data["STUDENT_NAME"],data["COURSENAME"],data["AGE"])
+#     cursor.execute(sql,values)
+#     connection.commit()
+#     return {
+#         "message":"Student Added sucessfully"
+#     }
+
+# def getStudent(id):
+#     sql = """select * from STUDENTS where id=%s"""
+#     value = id
+#     cursor.execute(sql,value)
+#     return cursor.fetchone()
+
+# def updateStudent(id):
+#     data = request.json
+#     sql = """Updtae STUDENTS set COURSENAME=%s,AGE=%s where id = %s"""
+#     cursor.execute(sql,data["COURSENAME"],data["AGE"],id)
+#     connection.commit()
+#     return { "message":"Updated succesfully"}
+
+# def deleteStudent(id):
+#     cursor.execute("Delte From STUDENTS where id=%s",(id))
+#     connection.commit()
+#     return{
+#         "message":"Student Removed Sucessfully"
+#     }
+
+
+
+
+from flask import request,render_template,redirect,url_for
 from dataBase.db import cursor,connection
 
 def getStudents():
     cursor.execute("select * From STUDENTS")
-    return cursor.fetchall()
+    stu =  cursor.fetchall()
+    return render_template(
+        "index.html",
+        stu=stu
+    )
 
 def addStudent():
-    data = request.get_json()
+    studentName = request.form["studentName"]
+    courseName = request.form["courseName"]
+    StudentAge = request.form["StudentAge"]
     sql = """INSERT INTO STUDENTS(STUDENT_NAME,COURSENAME,AGE) VALUES (%s,%s,%s)"""
-    values = (data["STUDENT_NAME"],data["COURSENAME"],data["AGE"])
+    values = (studentName,courseName,StudentAge)
     cursor.execute(sql,values)
     connection.commit()
-    return {
-        "message":"Student Added sucessfully"
-    }
+    return redirect(url_for("students.getAll"))
+
 
 def getStudent(id):
     sql = """select * from STUDENTS where id=%s"""
     value = id
     cursor.execute(sql,value)
     return cursor.fetchone()
+
+def editStudent(id):
+    sql = """select * from STUDENTS where id=%s"""
+    student = cursor.execute(sql,(id,))
+    student =  cursor.fetchone()
+    return render_template("edit.html",student=student)
+
+def updateStudent(id):
+    studentName = request.form["studentName"]
+    studentCourse = request.form["studentCourse"]
+    studentAge = request.form["studentAge"]
+    sql = """Update STUDENTS set studentName = %s COURSENAME=%s,AGE=%s where id = %s"""
+    cursor.execute(sql,studentName,studentCourse,studentAge,id)
+    connection.commit()
+    return redirect(url_for("students.getAll"))
+
+
+def deleteStudent(id):
+    cursor.execute("Delete From STUDENTS where id=%s",(id))
+    connection.commit()
+    if request.method == "GET": 
+        return redirect("/") 
+    
--- PyTier2/CrudApp/routes/studentRoutes.py
+++ PyTier2/CrudApp/routes/studentRoutes.py
@@ -7,6 +7,10 @@ students = Blueprint("students",__name__)
 def getAll():
     return getStudents()
 
+@students.route("/addPage",methods=["GET"])
+def addPage():
+    return render_template("add.html")
+
 @students.route("/add",methods=['POST'])
 def add_Student():
     return addStudent()
@@ -14,3 +18,14 @@ def add_Student():
 @students.route("/stu/<int:id>",methods=["GET"])
 def getOne(id):
     return getStudent(id)
+
+@students.route("/edit/<int:id>",methods=["GET"])
+def edit(id):
+    return editStudent(id)
+
+@students.route("/stuUpdate/<int:id>",methods=["PUT"])
+def update(id):
+    return updateStudent(id)
+@students.route("/delStu/<int:id>",methods=["DELETE","GET"])
+def deleteStu(id):
+    return deleteStudent(id)
